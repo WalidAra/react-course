8d8dbd3dc92b52dfc8e659dc8b318a2ac8829df3
WalidAra <arawalid90@gmail.com>
2024-06-10 19:55:37 +0100

    set up

--- chapter 1/day 6/src/config/firebase.js
+++ chapter 1/day 6/src/config/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtz1EGp85ygYPcUd12m2nHwILxcztI6B0",
-  authDomain: "nerd-202e5.firebaseapp.com",
-  projectId: "nerd-202e5",
-  storageBucket: "nerd-202e5.appspot.com",
-  messagingSenderId: "616560504639",
-  appId: "1:616560504639:web:e64f00b1a0cd2a6195ed8a",
-  measurementId: "G-BKC5KT9YBR",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
